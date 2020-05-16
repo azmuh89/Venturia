45dcbd9eaff6e17efdca85dc9aed4be92a8535cb
Humza Khan <humza.khan@msn.com>
2020-05-15 21:41:25 -0400

    Initialization

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float moveSpeed = 3f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+    bool running = false;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        Movement();
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (Input.GetKey("left shift") && !running)
+        {
+            running = true;
+            moveSpeed = 10;
+        }
+        else
+        {
+            running = false;
+            moveSpeed = 3;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void Movement()
+    {
+        if (movement.x != 0)
+        {
+            movement.y = 0;
+        }
+        else if (movement.y != 0)
+        {
+            movement.x = 0;
+        }
+
+        if (movement.x > 0)
+        {
+            animator.SetBool("MovingRight", true);
+            animator.SetBool("MovingLeft", false);
+            animator.SetBool("MovingUp", false);
+            animator.SetBool("MovingDown", false);
+        }
+        else if (movement.x < 0)
+        {
+            animator.SetBool("MovingLeft", true);
+            animator.SetBool("MovingUp", false);
+            animator.SetBool("MovingDown", false);
+            animator.SetBool("MovingRight", false);
+        }
+        else if (movement.y > 0)
+        {
+            animator.SetBool("MovingUp", true);
+            animator.SetBool("MovingLeft", false);
+            animator.SetBool("MovingDown", false);
+            animator.SetBool("MovingRight", false);
+        }
+        else if (movement.y < 0)
+        {
+            animator.SetBool("MovingDown", true);
+            animator.SetBool("MovingRight", false);
+            animator.SetBool("MovingLeft", false);
+            animator.SetBool("MovingUp", false);
+        }
+    }
+}
