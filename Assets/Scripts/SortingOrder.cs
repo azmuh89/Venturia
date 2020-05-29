c36d1d5eea5e5da8afbd68e1e11f1aaebad0fb1a
Humza Khan <humza.khan@msn.com>
2020-05-28 21:49:30 -0400

    Created Sorting order script
    It handles the sorting order of objects when player walks in front or behind them

---
+++ Assets/Scripts/SortingOrder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingOrder : MonoBehaviour
+{
+    public Transform player;
+    public int sortingOrder;
+    public int newOrder;
+    public float yLimit;
+
+    SpriteRenderer spriteRenderer;
+    float tempPos;
+    
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        tempPos = this.transform.position.y;
+    }
+    
+    void Update()
+    {
+        if (player.transform.position.y > tempPos + yLimit)
+        {
+            spriteRenderer.sortingOrder = newOrder;
+        }
+        else
+        {
+            spriteRenderer.sortingOrder = sortingOrder;
+        }
+    }
+}
