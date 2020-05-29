99cbaa0451c8dd8da5230031099bbb55c6dfd5d5
Humza Khan <humza.khan@msn.com>
2020-05-29 11:22:25 -0400

    Configured dialogue start on trigger

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject DialogueBox;
+    private Queue<string> sentences;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+    
+    void Update()
+    {
+    }
+    
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting conversation with: " + dialogue.name);
+
+        DialogueBox.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.transform.tag == "Player" && Input.GetKeyDown(KeyCode.Space))
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        }
+    }
+}
