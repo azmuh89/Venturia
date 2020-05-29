e60382c8fffb325d96bc8da76a90752b511b3190
Humza Khan <humza.khan@msn.com>
2020-05-28 20:37:26 -0400

    Fixed and changed tiles

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
+    public float ClampLeft;
+    public float ClampRight;
+    public float ClampDown;
+    public float ClampUp;
 
     void FixedUpdate()
     {
         transform.position = new Vector3(
-            Mathf.Clamp(player.position.x, -5f, 5f),
-            Mathf.Clamp(player.position.y, -5f, 5f),
+            Mathf.Clamp(player.position.x, -ClampLeft, ClampRight),
+            Mathf.Clamp(player.position.y, -ClampDown, ClampUp),
             transform.position.z);
     }
 }
