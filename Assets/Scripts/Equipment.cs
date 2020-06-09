b1ab0baa451dfc7b5969f375fbf09bc3c507d590
Humza Khan <humza.khan@msn.com>
2020-06-09 15:24:37 -0400

    fixed sword starting animation

---
+++ Assets/Scripts/Equipment.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equipment : PlayerMovement
+{
+    //void OnEnable()
+    //{
+    //    animator.SetBool("MovingDown", true);
+    //}
+}
