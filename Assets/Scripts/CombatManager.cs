7624f466a58832cedb10a8488c36fcb0a2ee6f70
Humza Khan <humza.khan@msn.com>
2020-06-24 21:25:39 -0400

    Fixed text boxes in canvases

---
+++ Assets/Scripts/CombatManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void Skills()
+    {
+
+    }
+
+    public void Spells()
+    {
+
+    }
+
+    public void Items()
+    {
+
+    }
+
+    public void Escape()
+    {
+
+    }
+}
