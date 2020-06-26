679fd639e2cfbda32a243cf8c286d6523ee871a4
Humza Khan <humza.khan@msn.com>
2020-06-25 21:48:39 -0400

    Updated main menu UI

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,15 +8,12 @@ using UnityEngine.EventSystems;
 public class MainMenu : MonoBehaviour
 {
     public GameObject settingsPanel;
-    public Toggle fullScreen;
-    public Dropdown resolutionDropdown;
+    public GameObject buttonsPanel;
     public Button playButton;
     public Button settingsButton;
 
     void Awake()
     {
-        fullScreen.isOn = intToBool(PlayerPrefs.GetInt("FullScreen"));
-        
         EventSystem.current.SetSelectedGameObject(playButton.gameObject);
         playButton.OnSelect(new BaseEventData(EventSystem.current));
     }
@@ -28,6 +25,7 @@ public class MainMenu : MonoBehaviour
             if (settingsPanel.activeInHierarchy)
             {
                 settingsPanel.SetActive(false);
+                buttonsPanel.SetActive(true);
                 EventSystem.current.SetSelectedGameObject(settingsButton.gameObject);
             }
         }
@@ -43,56 +41,5 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
-    }
-
-    public void FullScreenToggle(bool isFullScreen)
-    {
-        Screen.fullScreen = isFullScreen;
-
-        PlayerPrefs.SetInt("FullScreen", boolToInt(isFullScreen));
-    }
-
-    public void ScreenResolution()
-    {
-        if (resolutionDropdown.value == 0)
-        {
-            Screen.SetResolution(1920, 1080, fullScreen.isOn);
-        }
-        else if (resolutionDropdown.value == 1)
-        {
-            Screen.SetResolution(1600, 900, fullScreen.isOn);
-        }
-        else if (resolutionDropdown.value == 2)
-        {
-            Screen.SetResolution(1440, 900, fullScreen.isOn);
-        }
-        else if (resolutionDropdown.value == 3)
-        {
-            Screen.SetResolution(1366, 768, fullScreen.isOn);
-        }
-        else if (resolutionDropdown.value == 4)
-        {
-            Screen.SetResolution(1280, 720, fullScreen.isOn);
-        }
-        else if (resolutionDropdown.value == 5)
-        {
-            Screen.SetResolution(1024, 768, fullScreen.isOn);
-        }
-    }
-
-    int boolToInt(bool val)
-    {
-        if (val)
-            return 1;
-        else
-            return 0;
-    }
-
-    bool intToBool(int val)
-    {
-        if (val != 0)
-            return true;
-        else
-            return false;
     }
 }
