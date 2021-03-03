19e893d0b2bd7320ed0dc704768ffe7e65d02a2b
azmuh89 <54997838+azmuh89@users.noreply.github.com>
2021-03-03 14:05:07 -0500

    Updated Combat

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -8,8 +8,7 @@ using UnityEngine.SceneManagement;
 public class CombatManager : MonoBehaviour
 {
     public GameObject player;
-    public GameObject canvas;
-    public GameObject skillsSubButton, spellsSubButton;
+    public GameObject endPanel;
     public GameObject[] enemySpawnPoint;
 
     public GameObject swordSlash;
@@ -17,7 +16,7 @@ public class CombatManager : MonoBehaviour
     
     private GameObject[] enemies;
     private GameObject buttonChoices;
-    private GameObject endPanel;
+    private GameObject buttonMenu;
     private Button[] enemyName;
     private Vector3 playerPos;
     private Vector3[] enemyPos;
@@ -25,12 +24,6 @@ public class CombatManager : MonoBehaviour
     private EnemyController enemy;
     private PlayerStats playerStats;
     private CombatEnemy enemyStats;
-
-    private Button attackButton, skillsButton,
-        defendButton, itemsButton, escapeButton;
-
-    private GameObject attackView, skillsView,
-        defendView, itemsView, escapeView;
 
     private int enemyChoice;
     private bool attacking;
@@ -45,6 +38,9 @@ public class CombatManager : MonoBehaviour
         playerStats = FindObjectOfType<PlayerStats>();
 
         playerPos = player.transform.position;
+
+        buttonChoices = GameObject.Find("Choices");
+        buttonMenu = GameObject.Find("AttackView");
 
         //System.Array.Reverse(enemies);
 
@@ -66,9 +62,6 @@ public class CombatManager : MonoBehaviour
         }
 
         enemyStats = FindObjectOfType<CombatEnemy>();
-
-        FindButtons();
-        FindMenus();
     }
 
     void Start()
@@ -78,42 +71,6 @@ public class CombatManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (EventSystem.current.currentSelectedGameObject == skillsSubButton ||
-            EventSystem.current.currentSelectedGameObject == spellsSubButton)
-            {
-                EventSystem.current.SetSelectedGameObject(skillsButton.gameObject);
-            }
-
-            for (int i = 0; i < enemies.Length; i++)
-            {
-                if (EventSystem.current.currentSelectedGameObject == enemyName[i].gameObject)
-                {
-                    for (int j = 0; j < enemies.Length; j++)
-                    {
-                        enemyName[j].gameObject.SetActive(false);
-                    }
-
-                    canvas.SetActive(true);
-                    EventSystem.current.SetSelectedGameObject(attackButton.gameObject);
-                }
-            }
-        }
-
-        NavigationView();
-
-        if (!attacking)
-        {
-            if (!canvasActive)
-            {
-                canvas.SetActive(true);
-
-                EventSystem.current.SetSelectedGameObject(attackButton.gameObject);
-                canvasActive = true;
-            }
-        }
-
         if (swordSlashClone != null)
         {
             swordSlashClone.transform.position = Vector3.MoveTowards(swordSlashClone.transform.position, enemySpawnPoint[enemyChoice].transform.position, 50 * Time.deltaTime);
@@ -144,6 +101,12 @@ public class CombatManager : MonoBehaviour
         }
         else
         {
+            if (enemy.enemyCount > 0)
+            {
+                buttonChoices.SetActive(true);
+                buttonMenu.SetActive(true);
+                EventSystem.current.SetSelectedGameObject(GameObject.Find("AttackButton"));
+            }
             playerTurn = true;
             attacking = false;
         }
@@ -193,7 +156,8 @@ public class CombatManager : MonoBehaviour
             EventSystem.current.SetSelectedGameObject(enemyName[3].gameObject);
         }
 
-        canvas.SetActive(false);
+        buttonChoices.SetActive(false);
+        buttonMenu.SetActive(false);
     }
 
     public void AttackEnemy(int enemy)
@@ -243,75 +207,14 @@ public class CombatManager : MonoBehaviour
     {
         StopAllCoroutines();
 
-        canvas.SetActive(true);
-        buttonChoices.SetActive(false);
-        attackView.SetActive(false);
         endPanel.SetActive(true);
 
         //playerStats.experience += enemyStats.dropExperience;
-        expText.text = "Gained " + enemyStats.dropExperience + " Experience";
+        //expText.text = "Gained " + enemyStats.dropExperience + " Experience";
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene("PathToForest");
         }
-    }
-
-    void NavigationView()
-    {
-        if (EventSystem.current.currentSelectedGameObject == attackButton.gameObject)
-        {
-            SelectedButton(attackView);
-        }
-        else if (EventSystem.current.currentSelectedGameObject == skillsButton.gameObject)
-        {
-            SelectedButton(skillsView);
-        }
-        else if (EventSystem.current.currentSelectedGameObject == defendButton.gameObject)
-        {
-            SelectedButton(defendView);
-        }
-        else if (EventSystem.current.currentSelectedGameObject == itemsButton.gameObject)
-        {
-            SelectedButton(itemsView);
-        }
-        else if (EventSystem.current.currentSelectedGameObject == escapeButton.gameObject)
-        {
-            SelectedButton(escapeView);
-        }
-    }
-
-    void SelectedButton(GameObject activeObject)
-    {
-        attackView.SetActive(false);
-        skillsView.SetActive(false);
-        defendView.SetActive(false);
-        itemsView.SetActive(false);
-        escapeView.SetActive(false);
-
-        activeObject.gameObject.SetActive(true);
-    }
-
-    void FindButtons()
-    {
-        endPanel = canvas.transform.Find("EndScreen").gameObject;
-        expText = endPanel.transform.Find("ExpText").GetComponent<Text>();
-        itemsText = endPanel.transform.Find("ItemText").GetComponent<Text>();
-
-        buttonChoices = canvas.transform.Find("Choices").gameObject;
-        attackButton = buttonChoices.transform.Find("AttackButton").GetComponent<Button>();
-        skillsButton = buttonChoices.transform.Find("SkillsButton").GetComponent<Button>();
-        defendButton = buttonChoices.transform.Find("DefendButton").GetComponent<Button>();
-        itemsButton = buttonChoices.transform.Find("ItemsButton").GetComponent<Button>();
-        escapeButton = buttonChoices.transform.Find("EscapeButton").GetComponent<Button>();
-    }
-
-    void FindMenus()
-    {
-        attackView = canvas.transform.Find("AttackView").gameObject;
-        skillsView = canvas.transform.Find("SkillsView").gameObject;
-        defendView = canvas.transform.Find("DefendView").gameObject;
-        itemsView = canvas.transform.Find("ItemsView").gameObject;
-        escapeView = canvas.transform.Find("EscapeView").gameObject;
     }
 }
